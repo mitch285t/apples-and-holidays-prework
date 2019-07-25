7f1fd95d273c7920bed6f675f70478bf8735c983
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-25 20:30:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,6 +19,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # }
   # return the second element in the 4th of July array
 end
+def 
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
